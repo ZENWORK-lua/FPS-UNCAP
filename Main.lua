1b94a755d0a827b32b4fc2ef7d9fb8f4d07a02db
ZENWORK-lua <theperfectmanisgoku@gmail.com>
2026-09-09 03:48:10 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,7 @@
 -- [[ HYPER|HUB - STABLE ]]
+_G.HyperMainStart = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZENWORK-lua/Hyper-FPS-3D-Opening/refs/heads/main/Main.lua"))()
+repeat task.wait() until _G.HyperMainStart == true
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
